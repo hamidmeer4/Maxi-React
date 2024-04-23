76a581e426e3e63486726534782f24cc7b0efd89
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2024-04-23 22:43:27 +0500

    done with variable

---
+++ 01-JS-Refresher/Index.js
@@ -0,0 +1,82 @@
+// js can be excuted in different environment 
+// i ) in the browser
+// ii) on any computer > js code can excute outside of the browser
+// iii) you can build mobile app using js 
+
+
+
+
+// 2) adding js code to a website 
+// i) between script tag
+// ii) via import script 
+
+
+
+
+
+// 3) react project use a build process
+
+
+// 4) i) imoport and export 
+// ii ) default export
+// iii )  import * as util from './util.js
+
+
+
+
+
+// 5) Revisiting Variable & Values 
+
+// there are different type of value
+
+// i ) string  > text value 
+
+
+// const str = "helloo world "
+// console.log(str)
+
+
+// ii) Number > Positive or Negative 
+// with decimal is Float 
+// or without decimal point > Integer 
+
+
+
+// const age = 54;
+// console.log(age)
+
+
+
+
+// iii ) Boolean > True And False > means a simple yes or no type 
+// typically used in condition 
+
+// const age = 32;
+
+// if(age > 35) {
+//     console.log("the person to olld ")
+// }
+// else {
+//     console.log("Smart Person")
+// }
+
+
+// Null and Undefined 
+
+//  there is no Value 
+
+
+
+
+
+
+// Variable store value : variable is simply data container where u store value . 
+
+
+// why variable > readable and Reusable .
+
+
+
+
+
+
