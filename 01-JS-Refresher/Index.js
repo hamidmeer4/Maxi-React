4b949e1f47ea256fa746583b596d3492729a0080
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2024-04-24 12:07:00 +0500

    done with basics topic in JS-Refsher module

--- 01-JS-Refresher/Index.js
+++ 01-JS-Refresher/Index.js
@@ -77,6 +77,127 @@
 
 
 
+// Revisiting Operator > 
 
+// const a = 56;
+// const b = 32;
+
+// const c = a + b ;
+// console.log(c)
+
+// and so on ...
+
+
+// 7) 
+// Revisiting Operator 
+// function keyword 
+// function name 
+// parameter 
+// callng or invoke a functioon 
+// return type 
+// default value in parameter
+
+
+// 8)  Arrow function 
+
+// const CreateArrowFunction = () => {
+//     console.log("first")
+// }
+// CreateArrowFunction()
+
+
+
+
+
+// 11 ) Object > can be used to group multiple key value pairs 
+// > key will be upto u 
+
+// const obj = {
+//     name : "Hamid Meer",
+//     age : 23,
+// }
+
+// console.log(obj.age)
+
+
+// 12) Array > that can create a list of values 
+// array method > push ,indexOf , map > map can convert array to object 
+
+
+
+// 13 _) array destructuring > where we extrsct the element of the array and object 
+
+
+// 14 )  Spread Operator > The three dots will be pull out all the eleemnt of the Array 
+
+
+// const obj = {
+//     name : "Hamid",
+//     age : 23,
+//     address : "Darsamand"
+// }
+
+// const obj2= {
+//     ...obj,
+//     Educartion : "BSSE 6th SEMISTER"
+// }
+
+// console.log(obj2) // all the key value pair pull but in the object here 
+
+
+
+
+// Control Structure or Condition 
+
+
+// Dom Manipulation > Select element in the dom mabnually in this code to then read and chnage them 
+
+
+
+// Function as a value 
+
+
+// regular function 
+
+// function handleTimeOut () {
+//     console.log("TimeOut1")
+// }
+
+// const handleFunction2 = () => {
+//     console.log("TimeOut2")
+// }
+
+// setTimeout(handleTimeOut,2000)
+// setTimeout(handleFunction2,3000)
+
+// setTimeout(() => {
+//  console.log("More time out ")
+// },4000)
+
+
+
+
+// Define  a function > inside a funtion 
+
+// function init () {
+//     function greet() {
+//         console.log("Hey")
+//     }
+//     greet() // we can excute greet inside in init 
+// }
+// init() 
+
+
+// but we can't excute greet outside of init 
+
+
+
+// refrence and Primitve  
+
+// primitive > we can't edit them 
+
+// refrence > we can edit them
+
+// adress of data in memory 
 
 
