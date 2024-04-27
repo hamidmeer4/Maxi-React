b542597c3ac7b58d5ffc00902bc426515516eb89
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2024-04-28 00:18:48 +0500

    done with multiple state

--- 02-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 02-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -2,21 +2,61 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const [enterTitle, setEnterTitle] = useState('')
-    const [enterAmount, setEnterAmount] = useState('')
-    const [enterDate, setEnterDate] = useState('')
+  // const [enterTitle, setEnterTitle] = useState('')
+  // const [enterAmount, setEnterAmount] = useState('')
+  // const [enterDate, setEnterDate] = useState('')
+
+  // now we use Object for multiple state
+
+  const [enterInputUser, setenterUserInput] = useState({
+    enterTitle: "",
+    enterAmount: "",
+    enterDate: "",
+  });
 
   const titleChangeHandler = (e) => {
     setEnterTitle(e.target.value);
     console.log(e.target.value)
+
+    // for multiple state
+ 
+    // first way 
+    // setenterUserInput({
+    //   ...enterInputUser,
+    //   enterTitle: e.target.value,
+    // });
+
+
+    // or 
+    // second way 
+
+    // setenterUserInput((prevInput) => {
+    //     return {
+    //         ...prevInput,
+    //         enterAmount : e.target.value
+    //     }
+    // })
+
+
   };
+
   const amountChangeHandler = (e) => {
     setEnterAmount(e.target.value);
-    console.log(e.target.value)
+    console.log(e.target.value);
+
+//   2  setenterUserInput({
+//         ...enterInputUser,
+//         enterAmount : e.target.value
+//     })
   };
   const dateChangeHandler = (e) => {
-  setEnterDate(e.target.value);
-  console.log(e.target.value)
+    setEnterDate(e.target.value);
+    console.log(e.target.value);
+
+   // 2 setenterUserInput({
+    //     ...enterInputUser,
+    //     enterDate : e.target.value
+    // })
   };
   return (
     <form>
