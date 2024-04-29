6208639f7562d15ca8e3030618fcf3814cec5cb3
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2024-04-29 22:37:41 +0500

    we cover some topics alternative createing ashared handler function , Handler Form submisssion , Addinf two binding

--- 02-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 02-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -2,33 +2,32 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  // const [enterTitle, setEnterTitle] = useState('')
-  // const [enterAmount, setEnterAmount] = useState('')
-  // const [enterDate, setEnterDate] = useState('')
+  const [enterTitle, setEnterTitle] = useState("");
+  const [enterAmount, setEnterAmount] = useState("");
+  const [enterDate, setEnterDate] = useState("");
 
   // now we use Object for multiple state
 
-  const [enterInputUser, setenterUserInput] = useState({
-    enterTitle: "",
-    enterAmount: "",
-    enterDate: "",
-  });
+  //   const [enterInputUser, setenterUserInput] = useState({
+  //     enterTitle: "",
+  //     enterAmount: "",
+  //     enterDate: "",
+  //   });
 
   const titleChangeHandler = (e) => {
     setEnterTitle(e.target.value);
-    console.log(e.target.value)
+    console.log(e.target.value);
 
     // for multiple state
- 
-    // first way 
+
+    // first way
     // setenterUserInput({
     //   ...enterInputUser,
     //   enterTitle: e.target.value,
     // });
 
-
-    // or 
-    // second way 
+    // or
+    // second way
 
     // setenterUserInput((prevInput) => {
     //     return {
@@ -36,41 +35,77 @@ const ExpenseForm = () => {
     //         enterAmount : e.target.value
     //     }
     // })
-
-
   };
 
   const amountChangeHandler = (e) => {
     setEnterAmount(e.target.value);
     console.log(e.target.value);
 
-//   2  setenterUserInput({
-//         ...enterInputUser,
-//         enterAmount : e.target.value
-//     })
+    //   2  setenterUserInput({
+    //         ...enterInputUser,
+    //         enterAmount : e.target.value
+    //     })
   };
   const dateChangeHandler = (e) => {
     setEnterDate(e.target.value);
     console.log(e.target.value);
 
-   // 2 setenterUserInput({
+    // 2 setenterUserInput({
     //     ...enterInputUser,
     //     enterDate : e.target.value
     // })
   };
+
+  //   alternative  way a handler function
+
+  // const inputChangeHandler = (identifier,value) => {
+
+  //   if(identifier === 'title') {
+  //       setEnterTitle(value)
+  //   }
+  //   else if(identifier === 'date') {
+  //       setEnterDate(value)
+  //   }
+  //   else {
+  //       setEnterAmount(value)
+  //   }
+  // }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    //  creating a object for state here
+
+    const expenseData = {
+      title: enterTitle,
+      amount: enterAmount,
+      date: new Date(enterDate),
+    };
+    console.log(expenseData);
+    setEnterTitle("");
+    setEnterAmount("");
+    setEnterDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
           {/* Now we add event in our project  */}
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enterTitle} onChange={titleChangeHandler} />
+
+          {/* alternative way to create handler function  */}
+          {/* <input type="text" onChange = {(e) => inputChangeHandler('title',e.target.value) }> */}
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
+            value={enterAmount}
             onChange={amountChangeHandler}
+            // alternative way to create handler function
+            // {(e) => inputChangeHandler('title',e.target.value)
             min="0.01"
             step="0.01"
           />
@@ -79,6 +114,7 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
+            value={enterDate}
             onChange={dateChangeHandler}
             min="2020-01-01"
             max="2024-12-31"
